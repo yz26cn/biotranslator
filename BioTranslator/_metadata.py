2c9cd99d05729e8be2fd4f11164fb3cba6f803b9
Yunwei Zhao <yzhao4@uw.edu>
2022-08-26 09:26:33 -0700

    Edit

--- BioTranslator/_metadata.py
+++ BioTranslator/_metadata.py
@@ -1,7 +1,7 @@
 import traceback
 
 def within_flit():
-    """\
+    """
     Checks if we are being imported by flit.
     This is necessary so flit can import __version__ without all depedencies installed.
     There are a few options to make this hack unnecessary, see:
