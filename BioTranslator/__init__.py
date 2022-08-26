a960b918cdc993e11c8423374dde30781b1b4894
Yunwei Zhao <yzhao4@uw.edu>
2022-08-26 08:23:29 -0700

    Edit

--- BioTranslator/__init__.py
+++ BioTranslator/__init__.py
@@ -1,9 +1,5 @@
 """BioTranslator in Python."""
-__version__ = '0.1'
+__version__ = '0.1.1'
 
 from .biotranslator_function import setup_config, train_text_encoder, get_ontology_embeddings, train_biotranslator, \
     test_biotranslator
-from .biotranslator import BioTranslator
-from .config import BioConfig
-from .loader import BioLoader
-from .trainer import build_trainer
