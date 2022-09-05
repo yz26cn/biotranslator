8ffca04ebbc23fdb048721d0701f84019634fae1
Yunwei Zhao <yzhao4@uw.edu>
2022-09-05 02:03:51 -0700

    Update doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,12 +5,10 @@ copyright = f'{datetime.now():%Y}, BioTranslator team.'
 author = 'BioTranslator team'
 
 # -- General configuration ---------------------------------------------------
-extensions = ['myst-parser']
+extensions = [
+    'myst_parser'
+]
 
 templates_path = ['_templates']
 master_doc = 'index'
-pygments_style = 'sphinx'
 exclude_patterns = []
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
---
+++ docs/source/release_notes/release_latest.md
@@ -0,0 +1,4 @@
+# Version 0.1.0
+
+```{include} /release_notes/0.1.0.md
+```
